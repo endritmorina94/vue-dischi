d81826509fce6f7c9b61c5e2eb55895214dfb2d6
endritmorina94 <endrit.morina@outlook.it>
2021-04-28 13:35:23 +0200

    migliorato css + finito senza bonus

--- js/script.js
+++ js/script.js
@@ -2,10 +2,18 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+            dischi: []
         },
         methods: {
         },
         mounted() {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/array/music')
+                .then((response) => {
+                    const result = response.data;
+                    this.dischi = result.response;
+                    console.log(this.dischi);
+                });
         }
     }
 );
